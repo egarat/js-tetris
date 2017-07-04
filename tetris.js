3ddfa374cf02c693131580874609bd697ee751a5
Egarat <egarat.kaoroprat@gmail.com>
2017-07-04 16:37:43 +0200

    Add collision detection in the matrix

--- tetris.js
+++ tetris.js
@@ -9,6 +9,26 @@ const matrix = [
     [0, 1, 1]
 ];
 
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for(let y = 0; y < m.length; ++y) {
+        for(let x = 0; x < m[y].length; ++x) {
+            if(m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !==0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function createMatrix(w, h) {
+    const matrix = [];
+    while(h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -29,8 +49,23 @@ function drawMatrix(matrix, offset) {
     });
 }
 
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x)  => {
+            if(value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
+}
+
 function playerDrop() {
     player.pos.y++;
+    if(collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    }
     dropCounter = 0;
 }
 
@@ -50,6 +85,8 @@ function update(time = 0) {
     draw();
     requestAnimationFrame(update);
 }
+
+const arena = createMatrix(12, 20);
 
 const player = {
     pos: {x: 5, y: 5},
