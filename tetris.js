9173a24e0d1c078a839ae4c5ad1963a888225517
Egarat Kaoroprat <egarat.kaoroprat@gmail.com>
2017-07-04 23:33:47 +0200

    Add comments and fix formatting (2 spaces indentation)

--- tetris.js
+++ tetris.js
@@ -4,103 +4,108 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 const matrix = [
-    [0, 0, 0],
-    [1, 1, 0],
-    [0, 1, 1]
+  [0, 0, 0],
+  [1, 1, 0],
+  [0, 1, 1]
 ];
 
+// checks for collision of the tetris element
 function collide(arena, player) {
-    const [m, o] = [player.matrix, player.pos];
-    for(let y = 0; y < m.length; ++y) {
-        for(let x = 0; x < m[y].length; ++x) {
-            if(m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !==0) {
-                return true;
-            }
-        }
+  const [m, o] = [player.matrix, player.pos];
+  for(let y = 0; y < m.length; ++y) {
+    for(let x = 0; x < m[y].length; ++x) {
+      if(m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 function createMatrix(w, h) {
-    const matrix = [];
-    while(h--) {
-        matrix.push(new Array(w).fill(0));
-    }
-    return matrix;
+  const matrix = [];
+  while(h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
 }
 
+// renders the current situation of the canvas
 function draw() {
-    context.fillStyle = '#000';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-    drawMatrix(player.matrix, player.pos);
+  drawMatrix(player.matrix, player.pos);
 }
 
+// draws the tetris elements and its current position on the canvas
 function drawMatrix(matrix, offset) {
-    matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                context.fillStyle = 'red';
-                context.fillRect(x + offset.x,
-                                 y + offset.y,
-                                 1, 1);
-            }
-        });
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle = 'red';
+        context.fillRect(x + offset.x,
+                         y + offset.y,
+                         1, 1);
+        }
     });
+  });
 }
 
+// merges the situation of the game in a 2d array
 function merge(arena, player) {
-    player.matrix.forEach((row, y) => {
-        row.forEach((value, x)  => {
-            if(value !== 0) {
-                arena[y + player.pos.y][x + player.pos.x] = value;
-            }
-        });
-    });
+  player.matrix.forEach((row, y) => {
+      row.forEach((value, x)  => {
+        if(value !== 0) {
+          arena[y + player.pos.y][x + player.pos.x] = value;
+        }
+      });
+  });
 }
 
+// drops the tetris element
 function playerDrop() {
-    player.pos.y++;
-    if(collide(arena, player)) {
-        player.pos.y--;
-        merge(arena, player);
-        player.pos.y = 0;
-    }
-    dropCounter = 0;
+  player.pos.y++;
+  if(collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
 }
 
 let dropCounter = 0;
-let dropInterval = 1000;
+let dropInterval = 1000; // each second the tetris element falls down
 
 let lastTime = 0;
 function update(time = 0) {
-    const deltaTime = time - lastTime;
-    lastTime = time;
-    
-    dropCounter += deltaTime;
-    if(dropCounter > dropInterval) {
-        playerDrop();
-    }
+  const deltaTime = time - lastTime;
+  lastTime = time;
+  
+  dropCounter += deltaTime;
+  if(dropCounter > dropInterval) {
+    playerDrop();
+  }
 
-    draw();
-    requestAnimationFrame(update);
+  draw();
+  requestAnimationFrame(update);
 }
 
 const arena = createMatrix(12, 20);
 
 const player = {
-    pos: {x: 5, y: 5},
-    matrix: matrix,
+  pos: {x: 5, y: 5},
+  matrix: matrix,
 }
 
 document.addEventListener('keydown', event => {
-    if(event.keyCode === 37) {
-        player.pos.x--;
-    } else if(event.keyCode === 39) {
-        player.pos.x++;
-    } else if(event.keyCode === 40) {
-        playerDrop();
-    }
+  if(event.keyCode === 37) {
+      player.pos.x--;
+  } else if(event.keyCode === 39) {
+      player.pos.x++;
+  } else if(event.keyCode === 40) {
+      playerDrop();
+  }
 });
 
 update();
