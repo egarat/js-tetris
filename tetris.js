53e8b1708bb3874cb2ca83f78e809700438a986e
Egarat Kaoroprat <egarat.kaoroprat@gmail.com>
2017-07-06 23:45:10 +0200

    Add function to rotate Tetris element

--- tetris.js
+++ tetris.js
@@ -5,15 +5,15 @@ context.scale(20, 20);
 
 const matrix = [
   [0, 0, 0],
-  [1, 1, 0],
-  [0, 1, 1]
+  [1, 1, 1],
+  [0, 1, 0]
 ];
 
 // checks for collision of the tetris element
 function collide(arena, player) {
   const [m, o] = [player.matrix, player.pos];
-  for(let y = 0; y < m.length; ++y) {
-    for(let x = 0; x < m[y].length; ++x) {
+  for(let y = 0; y < m.length; y++) {
+    for(let x = 0; x < m[y].length; x++) {
       // if the tetris element reaches the end of the arena or there is already an element, return true
       if(m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
         return true;
@@ -36,7 +36,9 @@ function draw() {
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
+  // draw current state of the game
   drawMatrix(arena, {x: 0, y: 0})
+  // draw 'falling' tetris element
   drawMatrix(player.matrix, player.pos);
 }
 
@@ -84,6 +86,32 @@ function playerMove(direction) {
   }
 }
 
+// function to handle the rotation
+function playerRotate(direction) {
+  rotate(player.matrix, direction);
+}
+
+// function to rotate the tetris element
+function rotate(matrix, direction) {
+  for(let y = 0; y < matrix.length; y++) {
+    for(let x = 0; x < y; x++) {
+      [
+        matrix[x][y],
+        matrix[y][x]
+      ] = [
+        matrix[y][x],
+        matrix[x][y]
+      ];
+    }
+  }
+
+  if(direction > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000; // each second the tetris element falls down
 
@@ -115,6 +143,10 @@ document.addEventListener('keydown', event => {
       playerMove(1);
   } else if(event.keyCode === 40) {
       playerDrop();
+  } else if(event.keyCode === 81) {
+    playerRotate(-1);
+  } else if(event.keyCode === 87) {
+    playerRotate(1);
   }
 });
 
